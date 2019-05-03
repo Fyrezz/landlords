735875b6184fa0b63f55c000dde9d4200e1412af
Fyrezz <eduardo.sanchez.paz@gmail.com>
2019-05-03 16:31:48 +0200

    First

---
+++ src/main/java/net/fyrezz/me/landlords/P.java
@@ -0,0 +1,21 @@
+package net.fyrezz.me.landlords;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class P extends JavaPlugin {
+	
+	public static P p;
+	
+	public P() {
+		p = this;
+	}
+	
+	public void onEnable() {
+		
+	}
+	
+	public void onDisable() {
+		
+	}
+
+}
